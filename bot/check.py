50ed00b1e4f11f4df4662a60585632105a7e5180
A2577467404 <62649962+A2577467404@users.noreply.github.com>
2021-11-06 20:04:49 +0800

    Add files via upload

--- bot/check.py
+++ bot/check.py
@@ -1,15 +1,27 @@
 import jieba
 
-__RAW_CN__ = ['我操',
-              '你妈死了', 'NMSL', ' NM$L', 'nmzl', 'nm$l',
-              '傻逼', '伞兵', '申必', '啥b', 'sb', '杀币', '煞笔'
-              'nt', '脑瘫',
-              'esu', '恶俗', '户籍', '身份证号']
+__RAW_CN__ = ['我操', 'wc',   #No Fuck
+              '你妈死了', 'NMSL', ' NM$L', 'nmzl', 'nm$l',  #Mother
+              '傻逼', '伞兵', '申必', '啥b', 'sb', '杀币', '煞笔'  #idiot
+              'nt', '脑瘫',   #nt
+              'esu', '恶俗', '户籍', '身份证号',    #esu
+              '尸体', '死人']   #dead
 
-__RAW_EN__ = ['fuck',
-              'idolt',
-              'sexy',
-              'your mother dead']
+__RAW_EN__ = ['fuck', 'fk',     #fuck
+              'idiot',  #idiot
+              'sexy',   #yel
+              'your mother dead']   #dead
+
+__URL_BANNED__ = ['https://', 'http://', 'ftp://',  #qz
+                  '.com', '.top', '.io',    #domain
+                  '.cn', '.jp', '.us',  #country
+                  '.icu', '.earth'] #other
+
+__URL_ALLOW__ = ['baidu.com', '360.com', 'google.com', 'bing.com', 'shodan.io',     #Searcher
+                 'minecraft.net', 'steampowered.com',   #Game
+                 'microsoft.com', 'windows.com']    #Company
+
+__URL_BAN__ = ['taobao.com', 'sb666.com']
 
 __ERROR__ = ['Err0: Invalid Mode',
              'Err1: Invalid word list']
@@ -44,5 +56,24 @@ def text_check_self(text:str, raws:list):
         else:
             return False
 
+def check_url(text:str, bypass:bool=True):
+    if bypass:
+        for a in range(len(__URL_ALLOW__)):
+            if __URL_ALLOW__[a] in text:
+                return False
+
+    for banned in range(len(__URL_BANNED__)):
+        if __URL_BANNED__[banned] in text:
+            return True
+
+def check_url_banned(text:str):
+    for x in range(len(__URL_BAN__)):
+        if __URL_BAN__[x] in text:
+            return True
+
+    return False
+    pass
+
+
 if __name__ == '__main__':
-    print(text_check('You', 2))
+    print(check_url('You baidu.com'))
